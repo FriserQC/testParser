a23f48ed4357f6cbdae740bcd16f904171453799
PC <anthostag@gmail.com>
2023-02-10 18:14:55 -0500

    Parser Chemins

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -7,14 +8,14 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
-        NodeList configurationList = null;
-
         //Instancier la Factory qui permet d'accéder à un parser (appelé DocumentBuilder)
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        ArrayList<int[]> donneesChemin = new ArrayList<>();
 
         try {
             //Récupérer le parser
@@ -24,17 +25,16 @@ public class Main {
             Document doc = builder.parse("src\\configuration.xml");
 
             //Récupération d'un ensemble d'éléments ayant le même nom
-            configurationList = doc.getElementsByTagName("configuration");
-            Node config = configurationList.item(0);
-            if(config.getNodeType()==Node.ELEMENT_NODE){
-                NodeList metaSim = config.getChildNodes();
-                for (int j=0;j< metaSim.getLength();j++){
-                    Node meta = metaSim.item(j);
-                    if(meta.getNodeType()==Node.ELEMENT_NODE){
-                        NodeList usines = meta.getChildNodes();
-                        for(int i=0;i< usines.getLength();i++){
-                            Node usine = usines.item(i);
-                            System.out.println(usine.getNodeName());
+            NodeList listeChemins = doc.getElementsByTagName("simulation").item(0).getChildNodes();;
+            for(int i=0; i<listeChemins.getLength(); i++){
+                Node chemins = listeChemins.item(i);
+                if(chemins.getNodeType()==Node.ELEMENT_NODE && chemins.getNodeName()=="chemins"){
+                    NodeList chemin = chemins.getChildNodes();
+                    for (int j=0;j<chemin.getLength();j++){
+                        Node donnees = chemin.item(j);
+                        if(donnees.getNodeType()==Node.ELEMENT_NODE){
+                            Element elementDonnee = (Element) donnees;
+                            donneesChemin.add(new int[]{Integer.parseInt(elementDonnee.getAttribute("de")),Integer.parseInt(elementDonnee.getAttribute("vers"))});
                         }
                     }
                 }
