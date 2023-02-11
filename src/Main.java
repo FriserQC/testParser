432bc1447c9e39216ce4970769f7fb872db8714f
PC <anthostag@gmail.com>
2023-02-11 11:58:34 -0500

    Parser Usines complete

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
         //Instancier la Factory qui permet d'accéder à un parser (appelé DocumentBuilder)
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        ArrayList<int[]> donneesChemin = new ArrayList<>();
+        ArrayList<ArrayList<String[]>> donneesUsines = new ArrayList<>();
 
         try {
             //Récupérer le parser
@@ -29,39 +29,53 @@ public class Main {
             for(int i=0; i<usines.getLength(); i++){
                 Node usine = usines.item(i);
                 if(usine.getNodeType()==Node.ELEMENT_NODE && usine.getNodeName()=="usine"){
+                    ArrayList<String[]> u = new ArrayList<>();
+                    donneesUsines.add(u);
                     NodeList tUsine = usine.getChildNodes();
                     Element typeUsine = (Element) usine;
-                    System.out.println(typeUsine.getAttribute("type"));
+                    //System.out.println(typeUsine.getAttribute("type"));
                     for(int j=0; j<tUsine.getLength();j++){
                         Node donnees = tUsine.item(j);
                         if(donnees.getNodeType()==Node.ELEMENT_NODE && donnees.getNodeName()=="icones"){
                             NodeList icones = donnees.getChildNodes();
-                            System.out.println(donnees.getNodeName());
+                            //System.out.println(donnees.getNodeName());
                             for(int k=0; k<icones.getLength();k++){
                                 Node icone = icones.item(k);
                                 if(icone.getNodeType()==Node.ELEMENT_NODE){
                                     Element anIcone = (Element) icone;
-                                    System.out.println(anIcone.getAttribute("type"));
-                                    System.out.println(anIcone.getAttribute("path"));
+                                    //System.out.println(anIcone.getAttribute("type"));
+                                    //System.out.println(anIcone.getAttribute("path"));
+                                    u.add(new String[]{anIcone.getAttribute("type"), anIcone.getAttribute("path")});
                                 }
                             }
                         } if (donnees.getNodeType()==Node.ELEMENT_NODE && donnees.getNodeName()=="entree") {
                             Element entree = (Element) donnees;
-                            System.out.println(entree.getAttribute("type"));
+                            //System.out.println(entree.getAttribute("type"));
                             if(entree.getAttribute("capacite")==""){
-                                System.out.println(entree.getAttribute("quantite"));
+                                //System.out.println(entree.getAttribute("quantite"));
+                                u.add(new String[]{entree.getAttribute("type"),entree.getAttribute("quantite")});
                             }
                             else{
-                                System.out.println(entree.getAttribute("capacite"));
+                                //System.out.println(entree.getAttribute("capacite"));
+                                u.add(new String[]{entree.getAttribute("type"),entree.getAttribute("capacite")});
                             }
                         } if (donnees.getNodeType()==Node.ELEMENT_NODE && donnees.getNodeName()=="sortie") {
                             Element sortie = (Element) donnees;
-                            System.out.println(sortie.getAttribute("type"));
+                            //System.out.println(sortie.getAttribute("type"));
+                            u.add(new String[]{sortie.getAttribute("type")});
                         } if(donnees.getNodeType()==Node.ELEMENT_NODE && donnees.getNodeName()=="interval-production"){
-                            System.out.println("interval-production " + donnees.getTextContent());
+                            //System.out.println("interval-production " + donnees.getTextContent());
+                            u.add(new String[]{donnees.getTextContent()});
                         }
                     }
+                }
+            }
 
+            for(int i=0; i<donneesUsines.size();i++){
+                for(int j=0; j<donneesUsines.get(i).size();j++){
+                    for(int k=0;k<donneesUsines.get(i).get(j).length;k++){
+                        System.out.println(donneesUsines.get(i).get(j)[k]);
+                    }
                 }
             }
 
